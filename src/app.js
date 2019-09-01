1449dcaa8df698bd2c05e2b26248912f33a75709
Preston Chaplin <preston.chaplin@gmail.com>
2019-09-01 11:13:31 -0400

    updated 3d models with new font plus lighting updates

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
-
+import { RectAreaLightUniformsLib } from "three/examples/jsm/lights/RectAreaLightUniformsLib.js";
 
 class App extends Component {
   componentDidMount() {
@@ -20,7 +19,6 @@ class App extends Component {
 
     // ** Fog - exponentially denser further away from camera
     // this.scene.fog = new THREE.FogExp2( 0x000104, 0.01 );
-
 
     this.camera = new THREE.PerspectiveCamera(
       35,
@@ -46,93 +44,79 @@ class App extends Component {
   };
 
   lighting = () => {
-
     // *** add softbox ***
+
+    // Blue back
     RectAreaLightUniformsLib.init();
-		this.rectLight1 = new THREE.RectAreaLight( 0x0030ff, 3, 10, 10 );
-		this.rectLight1.position.set( 0, -1, -10 );
-    this.rectLight1.lookAt( 0, 0, 0 );
+    this.rectLight1 = new THREE.RectAreaLight(0x0030ff, 1, 5, 50);
+    this.rectLight1.position.set(0, 0, -10);
+    this.rectLight1.lookAt(0, 0, 0);
 
-   
-    const helper = new THREE.RectAreaLightHelper( this.rectLight1 );
-    // this.rectLight1.add( helper ); 
-		this.scene.add( this.rectLight1 );
+    this.scene.add(this.rectLight1);
 
-    // Blue
-    this.rectLight2 = new THREE.RectAreaLight( 0x0030ff, 3, 10, 10);
-    this.rectLight2.position.x = -8;
+    // Blue 2
+    this.rectLight2 = new THREE.RectAreaLight(0x0030ff, 1, 5, 50);
+    this.rectLight2.position.x = -9;
     this.rectLight2.position.z = 3;
-    this.rectLight2.add( helper2 ); 
-    const helper2 = new THREE.RectAreaLightHelper( this.rectLight2 );
+    this.rectLight2.lookAt(0, 0, 0);
     this.scene.add(this.rectLight2);
 
     // Red
-    this.rectLight3 = new THREE.RectAreaLight(0xfb3f3f, 3, 10, 10);
+    this.rectLight3 = new THREE.RectAreaLight(0xfb3f3f, 1, 5, 50);
     this.rectLight3.position.x = 8;
     this.rectLight3.position.z = 3;
-    this.rectLight3.add( helper3 );
-    const helper3 = new THREE.RectAreaLightHelper( this.rectLight3 );
+    this.rectLight3.lookAt(0, 0, 0);
     this.scene.add(this.rectLight3);
 
     // Green
-    this.rectLight4 = new THREE.RectAreaLight(0x18ff00, 3, 10, 10);
-    this.rectLight4.position.x = 16;
-    this.rectLight4.position.z = 3;
+    this.rectLight4 = new THREE.RectAreaLight(0x18ff00, 1, 5, 50);
+    this.rectLight4.position.x = -8;
+    this.rectLight4.position.z = -6;
+    this.rectLight4.lookAt(0, 0, 0);
 
-    this.rectLight4.add( helper4 );
-    const helper4 = new THREE.RectAreaLightHelper( this.rectLight4 );
     this.scene.add(this.rectLight4);
 
-    // //Cyan
-    // const pointLight4 = new THREE.RectAreaLight(0x00f6ff, 0.3);
-    // pointLight4.position.y = -8;
-    // pointLight4.position.z = 3;
-    // this.scene.add(pointLight4);
+    //Green 2
+    this.rectLight5 = new THREE.RectAreaLight(0x18ff00, 1, 10, 50);
+    this.rectLight5.position.set(0, 0, 10);
+    this.rectLight5.lookAt(0, 0, 0);
+    this.scene.add(this.rectLight5);
 
+    // Red 2
+    this.rectLight6 = new THREE.RectAreaLight(0xfb3f3f, 1, 15, 50);
+    this.rectLight6.position.x = -10;
+    this.rectLight6.position.z = 10;
+    this.rectLight6.lookAt(0, 0, 0);
+    this.scene.add(this.rectLight6);
 
-
-    // //Green
-    // const pointLight = new THREE.PointLight(0x1bc236, 0.3);
-    // pointLight.position.x = 8;
-    // pointLight.position.z = 3;
-    // this.scene.add(pointLight);
-
-    // //Red
-    // const pointLight2 = new THREE.PointLight(0xfb3f3f, 0.3);
-    // pointLight2.position.x = -8;
-    // pointLight2.position.z = 3;
-    // this.scene.add(pointLight2);
-
-    // //Blue
-    // const pointLight3 = new THREE.PointLight(0x0000ff, 0.3);
-    // pointLight3.position.y = 8;
-    // pointLight3.position.z = 3;
-    // this.scene.add(pointLight3);
-
-    // //Cyan
-    // const pointLight4 = new THREE.PointLight(0x00f6ff, 0.3);
-    // pointLight4.position.y = -8;
-    // pointLight4.position.z = 3;
-    // this.scene.add(pointLight4);
+    // const helper6 = new THREE.RectAreaLightHelper(this.rectLight6);
+    // this.rectLight6.add(helper6);
+    // const helper = new THREE.RectAreaLightHelper(this.rectLight1);
+    // this.rectLight1.add(helper);
+    // const helper2 = new THREE.RectAreaLightHelper(this.rectLight2);
+    // this.rectLight2.add(helper2);
+    // const helper3 = new THREE.RectAreaLightHelper(this.rectLight3);
+    // this.rectLight3.add(helper3);
+    // const helper4 = new THREE.RectAreaLightHelper(this.rectLight4);
+    // this.rectLight4.add(helper4);
+    // const helper5 = new THREE.RectAreaLightHelper(this.rectLight5);
+    // this.rectLight5.add(helper5);
   };
 
   addObjects = () => {
-
-    
-
     // ** Add Stars **
 
-  const starsGeometry = new THREE.Geometry();
-  for ( let i = 0; i < 100000; i ++ ) {
-	  let star = new THREE.Vector3();
-	  star.x = THREE.Math.randFloatSpread( 500 );
-	  star.y = THREE.Math.randFloatSpread( 500 );
-	  star.z = THREE.Math.randFloatSpread( 500 );
-	  starsGeometry.vertices.push( star );
-  }
-  const starsMaterial = new THREE.PointsMaterial( {size: 0.1} );
-  const starField = new THREE.Points( starsGeometry, starsMaterial );
-  // this.scene.add( starField );
+    // const starsGeometry = new THREE.Geometry();
+    // for (let i = 0; i < 100000; i++) {
+    //   let star = new THREE.Vector3();
+    //   star.x = THREE.Math.randFloatSpread(500);
+    //   star.y = THREE.Math.randFloatSpread(500);
+    //   star.z = THREE.Math.randFloatSpread(500);
+    //   starsGeometry.vertices.push(star);
+    // }
+    // const starsMaterial = new THREE.PointsMaterial({ size: 0.1 });
+    // const starField = new THREE.Points(starsGeometry, starsMaterial);
+    // this.scene.add( starField );
 
     // GLTF Loader
 
@@ -142,10 +126,14 @@ class App extends Component {
     const onLoad = gltf => {
       console.log(gltf);
       this.frame = gltf.scene.children[0];
-      this.frame.traverse ( ( o ) => {
-          if ( o.isMesh ) {
-              o.material = new THREE.MeshStandardMaterial({color: 0xfddf73, metalness: 0.7, roughness: 0.3});
-          }
+      this.frame.traverse(o => {
+        if (o.isMesh) {
+          o.material = new THREE.MeshStandardMaterial({
+            color: 0xfddf73,
+            metalness: 0.7,
+            roughness: 0.3
+          });
+        }
       });
       this.scene.add(this.frame);
     };
@@ -153,11 +141,15 @@ class App extends Component {
     const onLoad2 = gltf => {
       console.log(gltf);
       this.type = gltf.scene.children[0];
-      this.type.traverse ( ( o ) => {
-        if ( o.isMesh ) {
-            o.material = new THREE.MeshStandardMaterial({color: 0xfddf73, metalness: 0.7, roughness: 0.1});
-        }
+      this.type.traverse(o => {
+        if (o.isMesh) {
+          o.material = new THREE.MeshStandardMaterial({
+            color: 0xfddf73,
+            metalness: 0.7,
+            roughness: 0.1
           });
+        }
+      });
       this.scene.add(this.type);
     };
 
@@ -168,15 +160,16 @@ class App extends Component {
     };
 
     gltfLoader.load(
-      "/AppAge-stacked-07-solidDrilled.glb",
+      "/AppAge-Icon.glb",
       gltf => {
         onLoad(gltf);
       },
       onProgress,
       onError
     );
+
     gltfLoader2.load(
-      "/AppAge-stacked-Type.glb",
+      "/AppAge-3D.glb",
       gltf => {
         onLoad2(gltf);
       },
@@ -188,13 +181,11 @@ class App extends Component {
   animate = () => {
     requestAnimationFrame(this.animate);
     if (this.frame && this.type) {
-      // frame.rotation.x += 0.01;
       this.frame.rotation.y += 0.005;
       this.type.rotation.y += 0.005;
     }
 
-
-     // console.log(renderer.info.render.calls); 
+    // console.log(renderer.info.render.calls);
     this.renderer.render(this.scene, this.camera);
   };
 
